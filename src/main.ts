25647c8aa35a2fe68466272795575f7ec791190d
Alexandre Santos <alexandre.santana2201@gmail.com>
2026-01-02 14:58:10 -0300

    Configurações para utilização do Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,20 @@ async function bootstrap() {
     }),
   );
 
+  // Configuração do Swagger - Documentação da API
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API - Base')
+    .setDescription(
+      'Documentação da API, criada com NestJS para servir como base.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  // Criação do documento Swagger e setup do módulo Swagger
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // Inicia a aplicação na porta especificada ou na porta 3000 por padrão
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Nome do Usuário',
+    description: 'Nome completo do usuário',
+  })
   @IsString({ message: 'O nome deve ser um texto válido.' })
   @IsNotEmpty({ message: 'O nome é obrigatório.' })
   name: string;
 
+  @ApiProperty({ example: 25, description: 'Idade do usuário' })
   @IsInt({ message: 'A idade deve ser um número inteiro.' })
   @IsNotEmpty({ message: 'A idade é obrigatória.' })
   idade: number;
 
+  @ApiProperty({
+    example: 'email@dominio.com',
+    description: 'E-mail do usuário',
+  })
   @IsEmail({}, { message: 'O e-mail informado é inválido.' })
   @IsNotEmpty({ message: 'O e-mail é obrigatório.' })
   email: string;
